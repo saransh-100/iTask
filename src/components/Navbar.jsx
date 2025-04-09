44b8a5ec6d7c253a68614a566749dd918b0da0f7
saransh-100 <saranshkharel100@gmail.com>
2025-04-09 18:45:39 +1000

    Todo app done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ const Navbar = () => {
   return (
     <nav className="bg-violet-900 flex justify-between items-center text-white h-[8vh]">
       <div className="logo p-4">
-        <h1 className="text-4xl font-bold mx-8">iTask</h1>
+        <h1 className="text-2xl sm:text-4xl font-bold mx-4 sm:mx-8">iTask</h1>
       </div>
-      <div className="nav-links p-4">
-        <ul className="flex gap-8 mx-8">
+      <div className="nav-links p-3 sm:p-6">
+        <ul className="flex gap-4 sm:gap-8 mx-4 sm:mx-8 items-center">
           <li className='cursor-pointer hover:font-bold transition-all duration-100'>Home</li>
           <li className='cursor-pointer hover:font-bold transition-all duration-100'>Your Tasks</li>
         </ul>
